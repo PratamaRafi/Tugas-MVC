cea5a651f6b8dc1069af6fe961e4da94641ab8cb
Pratama Rafi <muh.rafi.pratama@gmail.com>
2020-07-01 09:29:24 +0700

    little change

--- KelasLaravel/resources/views/welcome.blade.php
+++ KelasLaravel/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <h1>SELAMAT DATANG {{$first_name}, {$last_name}}</h1>
+    <h1>SELAMAT DATANG {{$first_name}} {{$last_name}}</h1>
     <h2>Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</h2>
 </div>
